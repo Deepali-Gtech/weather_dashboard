bd4856315c8762a99637838bc08e88d32a285ad7
Deepali Garg <deepaligarg.chd@gmail.com>
2021-01-27 18:04:16 -0500

    added uv index and worked on forecast in html and js file

--- script.js
+++ script.js
@@ -5,63 +5,92 @@ $(document).ready(function () {
         var city = $("#city").val().trim();
         var lat;
         var lon;
-        if (city){
-        $.ajax({
-            url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&unit=imperial&appid=f1776acffaf07b5eb75b1acbf047c38b",
-            method: "GET",
-        }).then(function (response) {
-            console.log(response);
-            var city_date = response.name + "(" + getDate(response.dt) + ")";
-            $("#city-name").html(city_date);
-            $("#temp").html(response.main.temp + " F");
-            $("#humidity").html(response.main.humidity + " %");
-            $("#windSpeed").html(response.wind.speed + " MPH");
-
-            lat = response.coord.lat;
-            lon = response.coord.lon;
-
+        if (city) {
             $.ajax({
-                url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&unit=imperial&appid=f1776acffaf07b5eb75b1acbf047c38b",
+                url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&unit=imperial&appid=f1776acffaf07b5eb75b1acbf047c38b",
                 method: "GET",
             }).then(function (response) {
                 console.log(response);
-                var newDiv = $("<div>");
-                newDiv.html(city);
-                newDiv.css('border', 'thin solid black');
-                newDiv.css('text-align', 'center');
-                $("#city").val("");
-                $("#search-history").append(newDiv);
+                var city_date = response.name + "(" + getDate(response.dt) + ")";
+                $("#city-name").html(city_date);
+                $("#temp").html("Temperature: " +  response.main.temp + " F");
+                $("#humidity").html("Humidity: " + response.main.humidity + " %");
+                $("#windSpeed").html("Wind Speed: " +response.wind.speed + " MPH");
 
-                $("#date-1").html(getDate(response.daily[1].dt));
-                $("#max-temp-1").html("Temp: " + response.daily[1].temp.day + " F");
-                $("#max-humidity-1").html("Humidity: " + response.daily[1].humidity + " %");
+                lat = response.coord.lat;
+                lon = response.coord.lon;
 
-                $("#date-2").html(getDate(response.daily[2].dt));
-                $("#max-temp-2").html("Temp: " + response.daily[2].temp.day + " F");
-                $("#max-humidity-2").html("Humidity: " + response.daily[2].humidity + " %");
+                $.ajax({
+                    url: "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&unit=imperial&appid=f1776acffaf07b5eb75b1acbf047c38b",
+                    method: "GET",
+                }).then(function (response) {
+                    console.log(response);
+                    var newDiv = $("<div>");
+                    newDiv.html(city);
+                    newDiv.css('border', 'thin solid black');
+                    newDiv.css('text-align', 'center');
+                    $("#city").val("");
+                    $("#search-history").append(newDiv);
 
-                $("#date-3").html(getDate(response.daily[3].dt));
-                $("#max-temp-3").html("Temp: " + response.daily[3].temp.day + " F");
-                $("#max-humidity-3").html("Humidity: " + response.daily[3].humidity + " %");
+                    $("#uv-label").html("UV Index: ")
+                    $("#uvi").html(response.daily[0].uvi);
 
-                $("#date-4").html(getDate(response.daily[4].dt));
-                $("#max-temp-4").html("Temp: " + response.daily[4].temp.day + " F");
-                $("#max-humidity-4").html("Humidity: " + response.daily[4].humidity + " %");
+                    $("#uvi").css('background-color', 'red');
+                    
+                    var days = 5;
+                    var divForecastHeading = $("<h2>");
+                    divForecastHeading.html(days + "-Day Forecast");
+                    $("#main-section").append(divForecastHeading);
 
-                $("#date-5").html(getDate(response.daily[5].dt));
-                $("#max-temp-5").html("Temp: " + response.daily[5].temp.day + " F");
-                $("#max-humidity-5").html("Humidity: " + response.daily[5].humidity + " %");
+                    var divForecast = $("<div>");
+                    divForecast.addClass("row");
+                    $("#main-section").append(divForecast);
 
+                    var index = 1;
+                    while (index <= days) {
+                        var divOne = $("<div>");
+                        divOne.addClass("card text-white bg-primary mb-3 col-sm-2 px-2");
+                        divOne.css('max-width', '18rem');
+
+                        var divDate = getHtmlElement("<div>", "card-header", getDate(response.daily[index].dt));
+                        divOne.append(divDate);
+
+                        var divCardBody = $("<div>");
+                        divCardBody.addClass("card-body");
+
+                        var img = $("<img>");
+                        img.attr("src", "http://openweathermap.org/img/w/" + response.daily[index].weather[0].icon + ".png");
+                        divCardBody.append(img);
+
+                        var divTemp = getHtmlElement("<p>", "card-text", "Temp: " + response.daily[index].temp.day + " F");
+                        divCardBody.append(divTemp);
+
+                        var divHumidity = getHtmlElement("<p>", "card-text", "Humidity: " + response.daily[index].humidity + "%");
+                        divCardBody.append(divHumidity);
+
+                        divOne.append(divCardBody);
+
+                        divForecast.append(divOne);
+                        index++;
+                    }
+
+                });
             });
-        });
-    } else {
-        alert("Please enter city name.")
-    }
+        } else {
+            alert("Please enter city name.")
+        }
     });
 });
+
+function getHtmlElement(element, classes, html){
+    var element = $(element);
+    element.addClass(classes);
+    element.html(html);
+    return element;
+}
 
 function getDate(epochDate) {
     var date = new Date(0);
     date.setUTCSeconds(epochDate);
     return (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear();
-  }
+}
